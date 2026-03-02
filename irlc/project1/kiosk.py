18f7981097511825559bfc7494257979306b5517
AEPPLE <theapplejhon@gmail.com>
2026-03-02 22:32:14 +0100

    among us

--- irlc/project1/kiosk.py
+++ irlc/project1/kiosk.py
@@ -39,44 +39,46 @@ def warmup_states():
 def warmup_actions(): 
     return np.arange(0,15+1)
 
-def solve_kiosk_1(): 
-    N  = 14      # Horizon
-    ns = 20      # inventory Cap
-    no = 15      # Order Cap
-    co = 1.5     # Cost of Order
-    sp = 2.1     # Cost of Sell
-    
-    P_w = {0: 0.3, 3: 0.6, 6: 0.1} #PMF
+def solve_kiosk_1():
+    N = 14
+    ns = 20
+    no = 15
+    co = 1.5
+    sp = 2.1
+
+    P_W = {0: 0.3, 3: 0.6, 6: 0.1}
 
     S = list(range(ns + 1))
     A = list(range(no + 1))
 
-    for x in S:
-        J[N][x] = 0.0
+    J = [dict() for _ in range(N + 1)]
+    pi = [dict() for _ in range(N)]
 
-    for k in range(N - 1, -1, -1):
-        for x in S:
-            best_u = None
-            best_val = float("inf")
+    #Terminal cost
+    for x_N in S:
+        J[N][x_N] = 0.0
 
-            for u in U:
-                q = 0.0
-                for w, p in P_w.items():
-                    g = co * u - sp *  min(x + u, w)
-                    x_next = min(ns, max(0, x + u - w))
-                    q += p * (g + J[k + 1][x_next])
+    #Backwards propogation
+    for k in range(N - 1, -1, -1): #numerate backwards to 0
+        for x_k in S:
 
-                if q < best_val:
-                    best_val = q
-                    best_u = u
+            Q_k = {}
 
-            J[k][x] = best_val
-            pi[k][x] = best_u
+            for u_k in A:
+                val = 0.0
+
+                for w_k, p in P_W.items():
+                    x_k1 = min(ns, max(0, x_k + u_k - w_k))
+                    g_k = co * u_k - sp * min(x_k + u_k, w_k)
+                    val += p * (g_k + J[k + 1][x_k1])
+
+                Q_k[u_k] = val
+
+            u_star = min(Q_k, key=Q_k.get)
+            J[k][x_k] = Q_k[u_star]
+            pi[k][x_k] = u_star
 
     return J, pi
-
-    # TODO: 1 lines missing.
-    #raise NotImplementedError("Return cost and policy here (same format as DP_stochastic)")
 
 def solve_kiosk_2(): 
     # TODO: 1 lines missing.
